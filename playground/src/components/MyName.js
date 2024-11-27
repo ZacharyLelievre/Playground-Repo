7473e294cb7d2cc6ca005913a6fdbcf1a7aa2464
ZacharyLelievre <lelievrezachary@gmail.com>
2024-11-27 00:27:02 -0500

    infos componenent created

---
+++ playground/src/components/MyName.js
@@ -0,0 +1,11 @@
+
+const MyName = () => {
+    const infos = {"name": "Zachary", "age": 21};
+    return (
+        <div>
+            <h1>My name is {infos.name}</h1>
+            <h2>I am {infos.age}</h2>
+        </div>
+    )
+};
+export default MyName;
